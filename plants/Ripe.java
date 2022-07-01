debe10ecf6b721ebd60d2895c4aa9f983b9281dd
Валерия Шевченко <valeriya-shevchenko-2002@mail.ru>
2022-07-01 10:31:13 +0700

    Создание класса созревающих кустов

---
+++ plants/Ripe.java
@@ -0,0 +1,20 @@
+package potatobeetlesapp.plants;
+
+public class Ripe extends Plant {
+    
+    public Ripe() {
+        super();
+        setImage("/potatobeetlesapp/pictures/grass.png");
+    }
+    
+    @Override
+    public Plant grow() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Plant die() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
